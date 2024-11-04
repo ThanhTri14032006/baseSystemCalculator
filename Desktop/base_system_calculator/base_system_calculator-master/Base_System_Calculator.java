8e8a6eea604e6bceff89ad7068264bc6e8515bf3
tri <phamthanhtri60020341@gmail.com>
2024-11-04 20:47:43 +0700

    tri check

---
+++ Desktop/base_system_calculator/base_system_calculator-master/Base_System_Calculator.java
@@ -0,0 +1,18 @@
+public class Base_System_Calculator {
+    public static void main(String[] args) {
+        System.out.println("Chào mừng đến với DLP");
+        System.out.println("Sửa đổi lần 1!");
+        System.out.println("Sửa đổi lần 2!");
+        System.out.println("Sửa đổi lần 3!");
+        System.out.println("Sửa đổi lần 4!");
+        System.out.println("Chỉ Tuấn thay đổi dc");
+        System.out.println("Chỉ Tuấn thay đổi dc");
+        System.out.println(" tui thay roi tuan ");
+        System.out.println("Cuộc sống thì hữu hạn, chịu r Trí ơi");
+        System.out.println( " cuoc song huu han : ");
+        //tri check push đi pl !!!
+        // trả về !!
+        // trí phải làm lại .
+         System.out.println(" het cuu ");
+    }
+}
